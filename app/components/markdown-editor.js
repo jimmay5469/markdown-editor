1271f96e8072490402044f085f7e6a17543a1113
jimmay5469 <jimmay5469@gmail.com>
2016-03-18 14:51:12 -0400

    Iterate on the markdown editor

--- app/components/markdown-editor.js
+++ app/components/markdown-editor.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   title: 'An awesome post title',
-  markdown: 'Hello __world__!'
+  markdown: 'Hello __world__!',
+  showPreview: false,
+  actions: {
+    togglePreview() {
+      this.toggleProperty('showPreview');
+    }
+  }
 });
