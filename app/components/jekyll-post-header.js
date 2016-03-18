fbd9a12a482609a20c361954b7bd192856691aaa
jimmay5469 <jimmay5469@gmail.com>
2016-03-18 16:46:23 -0400

    Try out ember-computed-decorators

--- app/components/jekyll-post-header.js
+++ app/components/jekyll-post-header.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
+import computed from 'ember-computed-decorators';  // jshint ignore:line
 
 export default Ember.Component.extend({
   title: '',
-  date: Ember.computed(function() {
+
+  /* jshint ignore:start */
+  @computed
+  /* jshint ignore:end */
+  date() {
     return new Date();
-  }).volatile()
+  }
 });
--- app/components/markdown-renderer.js
+++ app/components/markdown-renderer.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
+import computed from 'ember-computed-decorators';  // jshint ignore:line
 import marked from 'npm:marked';
 
 export default Ember.Component.extend({
   value: '',
-  htmlValue: Ember.computed('value', function() {
-    return marked(this.get('value'));
-  })
+
+  /* jshint ignore:start */
+  @computed('value')
+  /* jshint ignore:end */
+  htmlValue(value) {
+    return marked(value);
+  }
 });
