295bd3a03f7ddf6a98a15b5d5eb375d2c5adb5b9
jimmay5469 <jimmay5469@gmail.com>
2016-03-18 15:10:18 -0400

    Add sass and make slight editor style improvements

--- app/components/markdown-editor.js
+++ app/components/markdown-editor.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['MarkdownEditorComponent'],
+
   title: 'An awesome post title',
   markdown: 'Hello __world__!',
   showPreview: false,
