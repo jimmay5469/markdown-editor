fda3c9a682ac7d3d4c430ea37766162888805801
jimmay5469 <jimmay5469@gmail.com>
2016-03-21 17:45:57 -0400

    Add autoprefixer

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    autoprefixer: {
+      browsers: ['last 2 versions', 'IE >= 10']
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
